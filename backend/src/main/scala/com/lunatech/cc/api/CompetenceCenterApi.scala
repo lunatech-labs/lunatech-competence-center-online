5742106810f3cbd0b497f94f48c217ef859a7e7e
Willem Jan Glerum <willem@wjglerum.nl>
2017-05-02 16:20:29 +0200

    Swap endpoints so /me also works

--- backend/src/main/scala/com/lunatech/cc/api/CompetenceCenterApi.scala
+++ backend/src/main/scala/com/lunatech/cc/api/CompetenceCenterApi.scala
@@ -23,7 +23,7 @@ object CompetenceCenterApi extends App {
 
   val cvController = new CVController(googleTokenVerifier, transactor)
 
-  val service = (cvController.`GET /employees` :+: cvController.`GET /employees/employeeId` :+: cvController.`GET /employees/me` :+: cvController.`PUT /employees/me` :+: cvController.`POST /cvs`).toServiceAs[Application.Json]
+  val service = (cvController.`GET /employees` :+: cvController.`GET /employees/me` :+: cvController.`GET /employees/employeeId` :+: cvController.`PUT /employees/me` :+: cvController.`POST /cvs`).toServiceAs[Application.Json]
 
   val corsService: Service[Request, Response] = new Cors.HttpFilter(policy).andThen(service)
 
