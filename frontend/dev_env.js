6110cc28e3d637e390ec7a79264bf2a50d3a2012
Erik Janssen <erikjanssen@erikwj.local>
2017-07-14 01:22:30 +0200

    some tryouts for local dev, no solution yet

---
+++ frontend/dev_env.js
@@ -0,0 +1,9 @@
+  EnvBehavior = {
+    properties: {
+      apiBaseUrl: {
+        type: String,
+        // value: '//some-url.com'     // production
+        value: 'http://localhost:8082' // development
+      }
+    }
+  };
