bad130a6c2cf3464e4d0e0b8301dc43bd5d7d9be
Erik Bakker <erik.bakker@lunatech.com>
2017-11-16 08:19:20 +0100

    First Jenkinsfile

--- scripts/deploy_jenkins.sh
+++ scripts/deploy_jenkins.sh
@@ -6,10 +6,14 @@ set -x
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
 
-mv backend/target/docker/stage/* .
+cd backend/target/docker/stage
 chmod 755 opt/docker/bin/*
 mkdir -p opt/docker/logs
 
 rm -rf target
+
+git init
 git add .
 git commit -m "Jenkins Build $BUILD_NUMBER"
+
+GIT_SSH_COMMAND="ssh -i $GIT_PRIVATE_KEY" git push --force $GIT_URL master
