658719d689c7571a64f3d8e17fe6f4b5d5b83f86
Jasper Dijt <jasper.dijt@lunatech.com>
2018-01-02 11:57:19 +0100

    Switch over polymer container to the UID of the user running it, to prevent permissions issues with bind mount.

--- scripts/cc_fullstack/scripts/start_frontend.sh
+++ scripts/cc_fullstack/scripts/start_frontend.sh
@@ -2,6 +2,7 @@
 
 set -x
 
-/root/scripts/link_local_polymer_components.sh
+/home/polymer/scripts/link_local_polymer_components.sh
 
-polymer serve --hostname 0.0.0.0 --port 8080 /root/frontend
+
+polymer serve --hostname 0.0.0.0 --port 8080 /home/polymer/frontend
