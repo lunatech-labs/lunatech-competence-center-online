ee46b02c84b62cf410aeba37582d593c2c36a8aa
Inaki De Tejada Plessmann <inaki.de.tejada@lunatech.com>
2018-07-20 14:58:28 +0200

    Endpoints for delivering the career framework levels

---
+++ backend/src/main/scala/com/lunatech/cc/api/CareerFrameworkController.scala
@@ -0,0 +1,27 @@
+package com.lunatech.cc.api
+
+import com.lunatech.cc.api.services.CareerFrameworkService
+import io.circe._
+import io.circe.generic.auto._
+import io.circe.syntax._
+import io.finch._
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory._
+
+class CareerFrameworkController(careerFrameworkService: CareerFrameworkService, authenticated: Endpoint[ApiUser], authenticatedUser: Endpoint[EnrichedGoogleUser]) {
+
+  lazy val logger: Logger = getLogger(getClass)
+
+  val `GET /career`: Endpoint[Json] = get("career" :: authenticatedUser) { (user: EnrichedGoogleUser) =>
+    for {
+      levels <- careerFrameworkService.findAll
+    } yield Ok(levels.asJson)
+  }
+
+  val `GET /career/{shortname}`: Endpoint[Json] = get("career" :: string :: authenticatedUser) { (shortname: String, user: EnrichedGoogleUser) =>
+    for {
+      level <- careerFrameworkService.findByShortName(shortname)
+    } yield Ok(level.asJson)
+  }
+
+}
