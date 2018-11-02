ebb423b39a40d7b7e4fe1e17f2b632c0a9d5042d
Nataliia Dziubenko <smthelusive@gmail.com>
2018-11-02 14:45:09 +0100

    a bit of refactoring (#186)

--- backend/src/main/scala/com/lunatech/cc/api/CoreCurriculumController.scala
+++ backend/src/main/scala/com/lunatech/cc/api/CoreCurriculumController.scala
@@ -1,16 +1,18 @@
 package com.lunatech.cc.api
 
 import com.lunatech.cc.api.services.CoreCurriculumService
+import com.lunatech.cc.api.services.CoreCurriculumService.{KnowledgeItem, ProjectItem}
 import com.twitter.util.Future
 import io.circe.Json
 import io.circe.generic.auto._
 import io.circe.syntax._
-import io.finch.Endpoint
-import io.finch._
+import io.finch.{Endpoint, _}
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory._
 
-class CoreCurriculumController(coreCurriculumService: CoreCurriculumService, authenticated: Endpoint[ApiUser], authenticatedUser: Endpoint[EnrichedGoogleUser]) {
+class CoreCurriculumController(coreCurriculumService: CoreCurriculumService,
+                               authenticated: Endpoint[ApiUser],
+                               authenticatedUser: Endpoint[EnrichedGoogleUser]) {
 
   lazy val logger: Logger = getLogger(getClass)
 
@@ -20,53 +22,61 @@ class CoreCurriculumController(coreCurriculumService: CoreCurriculumService, aut
     } yield Ok(summaries.asJson)
   }
 
-  val `GET /people/me/knowledge/{subject}`: Endpoint[Vector[String]] = get("people" :: "me" :: "knowledge" :: string :: authenticatedUser) { (subject: String, user: EnrichedGoogleUser) =>
+  val `GET /people/me/knowledge/{subject}`: Endpoint[Vector[String]] =
+    get("people" :: "me" :: "knowledge" :: string :: authenticatedUser) { (subject: String, user: EnrichedGoogleUser) =>
     for {
-      knowledge <- coreCurriculumService.getPersonKnowledge(user.email, subject)
+      knowledge <- coreCurriculumService.getPersonSubjectTopics(user.email, subject)
     } yield Ok(knowledge)
   }
 
-  val `GET /people/{email}/knowledge/{subject}`: Endpoint[Vector[String]] = get("people" :: string :: "knowledge" :: string :: authenticated) { (email: String, subject: String, user: ApiUser) =>
+  val `GET /people/{email}/knowledge/{subject}`: Endpoint[Vector[String]] =
+    get("people" :: string :: "knowledge" :: string :: authenticated) { (email: String, subject: String, user: ApiUser) =>
     if(user.hasRole("admin") || user.hasRole("mentor")) {
       for {
-        knowledge <- coreCurriculumService.getPersonKnowledge(email, subject)
+        knowledge <- coreCurriculumService.getPersonSubjectTopics(email, subject)
       } yield Ok(knowledge)
     } else {
       Future.value(Forbidden(new RuntimeException("'admin' or 'mentor' role required for this endpoint")))
     }
   }
 
-  val `PUT /people/me/knowledge/{subject}/{topic}`: Endpoint[Unit] = put("people" :: "me" :: "knowledge" :: string :: string :: authenticatedUser) { (subject: String, topic: String, user: EnrichedGoogleUser) =>
+  val `PUT /people/me/knowledge/{subject}/{topic}`: Endpoint[Unit] =
+    put("people" :: "me" :: "knowledge" :: string :: string :: authenticatedUser) { (subject: String, topic: String, user: EnrichedGoogleUser) =>
     for {
       _ <- coreCurriculumService.addPersonKnowledge(user.email, subject, topic)
     } yield Ok(())
   }
 
-  val `DELETE /people/me/knowledge/{subject}/{topic}`: Endpoint[Unit] = delete("people" :: "me" :: "knowledge" :: string :: string :: authenticatedUser) { (subject: String, topic: String, user: EnrichedGoogleUser) =>
+  val `DELETE /people/me/knowledge/{subject}/{topic}`: Endpoint[Unit] =
+    delete("people" :: "me" :: "knowledge" :: string :: string :: authenticatedUser) { (subject: String, topic: String, user: EnrichedGoogleUser) =>
     for {
       _ <- coreCurriculumService.removePersonKnowledge(user.email, subject, topic)
     } yield Ok(())
   }
 
-  val `GET /people/{email}/knowledge`: Endpoint[Map[String, Vector[Vector[String]]]] = get("people" :: string :: "knowledge" :: authenticated) { (person: String, apiUser: ApiUser) =>
+  val `GET /people/{email}/knowledge`: Endpoint[Vector[KnowledgeItem]] =
+    get("people" :: string :: "knowledge" :: authenticated) { (person: String, apiUser: ApiUser) =>
     for {
       knowledge <- coreCurriculumService.getAllPersonKnowledge(person)
     } yield Ok(knowledge)
   }
 
-  val `GET /people/{email}/projects`: Endpoint[Map[String, Vector[Vector[String]]]] = get("people" :: string :: "projects" :: authenticated) { (person: String, apiUser: ApiUser) =>
+  val `GET /people/{email}/projects`: Endpoint[Vector[ProjectItem]] =
+    get("people" :: string :: "projects" :: authenticated) { (person: String, apiUser: ApiUser) =>
     for {
       projects <- coreCurriculumService.getAllPersonProjects(person)
     } yield Ok(projects)
   }
 
-  val `GET /people/me/projects/{subject}`: Endpoint[Vector[Vector[String]]] = get("people" :: "me" :: "projects" :: string :: authenticatedUser) { (subject: String, user: EnrichedGoogleUser) =>
+  val `GET /people/me/projects/{subject}`: Endpoint[Vector[ProjectItem]] =
+    get("people" :: "me" :: "projects" :: string :: authenticatedUser) { (subject: String, user: EnrichedGoogleUser) =>
     for {
       projects <- coreCurriculumService.getPersonSubjectProjects(user.email, subject)
     } yield Ok(projects)
   }
 
-  val `GET /people/{email}/projects/{subject}`: Endpoint[Vector[Vector[String]]] = get("people" :: string :: "projects" :: string :: authenticated) { (email: String, subject: String, user: ApiUser) =>
+  val `GET /people/{email}/projects/{subject}`: Endpoint[Vector[ProjectItem]] =
+    get("people" :: string :: "projects" :: string :: authenticated) { (email: String, subject: String, user: ApiUser) =>
     if(user.hasRole("admin") || user.hasRole("mentor")) {
       for {
         projects <- coreCurriculumService.getPersonSubjectProjects(email, subject)
@@ -76,7 +86,8 @@ class CoreCurriculumController(coreCurriculumService: CoreCurriculumService, aut
     }
   }
 
-  val `PUT /people/me/projects/{subject}/{project}/{status}`: Endpoint[Unit] = put("people" :: "me" :: "projects" :: string :: string :: string :: authenticatedUser) {
+  val `PUT /people/me/projects/{subject}/{project}/{status}`: Endpoint[Unit] =
+    put("people" :: "me" :: "projects" :: string :: string :: string :: authenticatedUser) {
     (subject: String, project: String, status: String, user: EnrichedGoogleUser) => {
       if (status.equals("in-progress")) {
         for {
@@ -94,7 +105,8 @@ class CoreCurriculumController(coreCurriculumService: CoreCurriculumService, aut
     }
   }
 
-  val `DELETE /people/me/projects/{subject}/{project}`: Endpoint[Unit] = delete("people" :: "me" :: "projects" :: string :: string :: authenticatedUser) {
+  val `DELETE /people/me/projects/{subject}/{project}`: Endpoint[Unit] =
+    delete("people" :: "me" :: "projects" :: string :: string :: authenticatedUser) {
     (subject: String, project: String, user: EnrichedGoogleUser) =>
     for {
       _ <- coreCurriculumService.setProjectNotStarted(user.email, subject, project)
