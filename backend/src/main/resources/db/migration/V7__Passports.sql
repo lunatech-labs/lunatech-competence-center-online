fcb69ae3d6440f758bc2ce0b49167f51910d06d1
Ender Tunç <araknafobiatr@gmail.com>
2018-11-30 09:18:00 +0100

    enable passport page (#187)
    backend compiles again
    
    restructuring cv related endpoints
    
    skills from matrix
    
    wip
    
    passport stuff working
    
    remove debug
    
    fixed getting cvs with uuid
    
    fixing cvs by uuid
    
    swagger updates
    
    enable passport and cvs page
    
    Minor improvements and start to refactor employees page
    
    finalize passport page
    
    improve skill listing
    
    rename passport.sql and add index on person field

---
+++ backend/src/main/resources/db/migration/V7__Passports.sql
@@ -0,0 +1,13 @@
+-- Table: public.passports
+--
+
+CREATE TABLE passports (
+  id         UUID                                                NOT NULL,
+  created_on TIMESTAMP WITH TIME ZONE                            NOT NULL,
+  modified_on TIMESTAMP WITH TIME ZONE                           NOT NULL,
+  passport   JSON                                                NOT NULL,
+  person     CHARACTER VARYING(255)                              NOT NULL,
+  CONSTRAINT passports_pkey PRIMARY KEY (person)
+);
+
+CREATE INDEX idx_passport_person ON passports (person);
---
+++ backend/src/main/scala/com/lunatech/cc/api/PassportController.scala
@@ -0,0 +1,106 @@
+package com.lunatech.cc.api
+
+import com.lunatech.cc.api.services.{PassportService, PeopleService, Person, SkillMatrixService}
+import com.lunatech.cc.models.{Employee, Skill}
+import com.twitter.util.Future
+import Routes._
+import io.circe._
+import io.circe.generic.auto._
+import io.circe.syntax._
+import io.finch._
+import io.finch.circe._
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory._
+
+class PassportController(passportService: PassportService, peopleService: PeopleService,
+                         skillMatrixService: SkillMatrixService, authenticated: Endpoint[ApiUser],
+                         authenticatedUser: Endpoint[EnrichedGoogleUser]) {
+
+  lazy val logger: Logger = getLogger(getClass)
+
+  val `GET /passport/me`: Endpoint[Json] = get("passport" :: "me":: authenticatedUser) { (user: EnrichedGoogleUser) =>
+      logger.debug(s"GET /passport for $user")
+      getUpdatedPassportOrDefault(user.toPerson).map(Ok)
+  }
+
+  val `GET /passport/employeeId`: Endpoint[Json] = get("passport" :: string :: authenticated) { (employeeId: String, user: ApiUser) =>
+    logger.debug(s"GET /passport/$employeeId for $user")
+
+    val result: Future[Either[RuntimeException, Json]] = peopleService.findByEmail(employeeId).flatMap {
+      case Some(e) => getUpdatedPassportOrDefault(e).map(Right(_))
+      case None =>  Future.value(Left(new RuntimeException(s"No data found for $employeeId")))
+    }
+
+    result.map {
+      case Right(j) => Ok(j)
+      case Left(e) => NotFound(e)
+    }
+
+
+  }
+
+  val `PUT /passport`: Endpoint[Json] = put("passport" :: authenticatedUser :: jsonBody[Json]) { (user: EnrichedGoogleUser, passport: Json) =>
+    passport.as[Employee] match {
+      case Right(_) =>
+        logger.debug("received data")
+        val result: Int = passportService.save(user.email, passport)
+        if (result > 0) Ok(passport)
+        else InternalServerError(new Exception("Errors while saving passport"))
+      case Left(e) =>
+        logger.debug(s"incorrect data $e in $passport")
+        BadRequest(new RuntimeException(e))
+    }
+  }
+
+  /**
+    * Returns all developers with their CVs
+    */
+  val `GET /employees/passport`: Endpoint[Json] = get(employees :: "passport" :: authenticated) { (user: ApiUser) =>
+    logger.debug(s"GET /employees/passport by user $user")
+
+    val result: Future[Seq[Future[Json]]] =  for {
+      people <- peopleService.findByRole("developer")
+    } yield people.map(p => getUpdatedPassportOrDefault(p))
+
+    result.flatMap(d =>  {
+      Future.collect(d).map( r => {
+        Ok(r.asJson)
+      })
+    })
+
+  }
+
+  private def getUpdatedPassportOrDefault(p: Person) = {
+
+    val skills: Future[Seq[Skill]] = skillMatrixService.findByEmail(p.email).map(_.skills.map(_.toSkill))
+
+    skills.map { skls =>
+
+      passportService.findByEmail(p.email) match {
+        case Some(d) => {
+          logger.debug(s"GET /employees/passport finding passport for $d")
+
+          val pass: Employee = d.as[Employee] match {
+            case Left(e) =>
+              logger.warn(s"Error retrieving data ${e.message}, creating new Employee")
+              Employee.apply(p).updateSkills(skls)
+            case Right(e) =>
+              logger.debug(s"ok updating skills for ${e.basics.email}")
+              e.updateSkills(skls)
+          }
+          pass.asJson
+        }
+        case None => {
+          logger.debug(s"No data found for user ${p.email} and $skls")
+          Employee.apply(p).updateSkills(skls).asJson
+        }
+      }
+    }
+  }
+
+
+
+
+
+}
+
